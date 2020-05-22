bea43d7b92fe89a69f49a272f99c3e48d381638f
Cristhian Yeison <yeison.pachari.10@gmail.com>
2020-05-22 09:14:39 -0500

    pregunta5

---
+++ Pregunta5Python.py
@@ -0,0 +1,15 @@
+print("Introduce el sueldo")
+sueldo=input()
+print("Introduce años de trabajo")
+año=input()
+x=1
+if x <= año:
+    sueldo += (sueldo)*0.10
+    print("El sueldo en el año"),x,("es de"),sueldo
+    x=x+1
+
+
+
+
+
+
